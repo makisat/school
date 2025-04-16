a96359a7d2d834ef496c0954205e08ac71074cc3
makisat <makiya28sato@gmail.com>
2025-04-16 18:29:14 -0400

    hw8 done

--- cmsc313/hw8/main.cpp
+++ cmsc313/hw8/main.cpp
@@ -1,55 +1,97 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
 
 class Matrix {
 private:
   int rows;
   int cols;
   std::vector<std::vector<int>> data;
+
 public:
-  Matrix(int rows, int cols) : rows(rows), cols(cols) {
-    data.resize(rows);
-    for (auto& row : data) {
-      row.resize(cols, 0);
-    }
+  Matrix(int r, int c) : rows(r), cols(c), data(r, std::vector<int>(c, 0)) {}
+
+  Matrix(const std::vector<std::vector<int>>& init_data) {
+    rows = init_data.size();
+    cols = init_data[0].size();
+    data = init_data;
   }
 
-  void setData(const std::vector<std::vector<int>>& new_data) {
-    data = new_data;
-}
+  int getRows() const { return rows; }
+  int getCols() const { return cols; }
 
-  void showData() {
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
+  void showData() const {
+    for (int i = 0; i < rows; ++i) {
+      for (int j = 0; j < cols; ++j) {
         std::cout << data[i][j] << " ";
       }
       std::cout << std::endl;
     }
   }
+
+  Matrix transpose() const {
+    Matrix result(cols, rows);
+    for (int i = 0; i < rows; ++i) {
+      for (int j = 0; j < cols; ++j) {
+        result.data[j][i] = data[i][j];
+      }
+    }
+    return result;
+  }
+
+  friend Matrix operator+(const Matrix& a, const Matrix& b) {
+    Matrix result(a.rows, a.cols);
+    for (int i = 0; i < a.rows; ++i) {
+      for (int j = 0; j < a.cols; ++j) {
+        result.data[i][j] = a.data[i][j] + b.data[i][j];
+      }
+    }
+    return result;
+  }
+
+  friend Matrix operator*(int scalar, const Matrix& m) {
+    Matrix result(m.rows, m.cols);
+    for (int i = 0; i < m.rows; ++i) {
+      for (int j = 0; j < m.cols; ++j) {
+        result.data[i][j] = scalar * m.data[i][j];
+      }
+    }
+    return result;
+  }
+
+  friend Matrix operator*(const Matrix& a, const Matrix& b) {
+    Matrix result(a.rows, b.cols);
+    for (int i = 0; i < a.rows; ++i) {
+      for (int j = 0; j < b.cols; ++j) {
+        int sum = 0;
+        for (int k = 0; k < a.cols; ++k) {
+          sum += a.data[i][k] * b.data[k][j];
+        }
+        result.data[i][j] = sum;
+      }
+    }
+    return result;
+  }
 };
 
 int main() {
+  Matrix A({{6, 4}, {8, 3}});
+  Matrix B({{1, 2, 3}, {4, 5, 6}});
+  Matrix C({{2, 4, 6}, {1, 3, 5}});
 
-  std::vector<std::vector<int>> data_A =
-  { {6, 4},
-    {8, 3} };
-
-  std::vector<std::vector<int>> data_B =
-  { {1, 2, 3},
-    {4, 5, 6} };
-
-  Matrix A = Matrix(2, 2);
-  A.setData(data_A);
-  Matrix B = Matrix(2, 3);
-  B.setData(data_B);
-
-  std::cout << "data A" << std::endl;
+  std::cout << "A:" << std::endl;
   A.showData();
-
-  std::cout << "data B" << std::endl;
+  std::cout << "\nB:" << std::endl;
   B.showData();
+  std::cout << "\nC:" << std::endl;
+  C.showData();
+
+  Matrix T1 = 3 * B;
+  Matrix T2 = C.transpose();
+  Matrix T3 = T1 * T2;
+  Matrix D = A + T3;
+
+  std::cout << "\nD = A + (3 * B) * C^T:" << std::endl;
+  D.showData();
 
   return 0;
 }
-
-
